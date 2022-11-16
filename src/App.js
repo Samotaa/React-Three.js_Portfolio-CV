df9132dc42d9f7148012bebccb5b91fcb607e490
ALWAYSWANNATILT <roflanbuldiga007@mail.ru>
2022-11-16 20:50:59 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './App.scss';
+import Fullpage, {FullPageSections, FullpageSection, FullpageNavigation} from '@ap.cx/react-fullpage'
 import Second from './components/SecondSection/SecondSection'
 import First from './components/FirstSection/FirstSection'
 import Third from './components/ThirdSection/ThirdSection'
+
+
 function App() {
   return (
     <div> 
